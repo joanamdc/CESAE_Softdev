884180c6e09c94d75a5436f975c4526af47fa7d2
joanamdc <148441179+joanamdc@users.noreply.github.com>
2023-10-30 16:14:31 +0000

    aula
    aula

--- FichasJava/src/FichaPratica03/Ex_13.java
+++ FichasJava/src/FichaPratica03/Ex_13.java
@@ -6,7 +6,14 @@ public class Ex_13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int numero;
+        int numAnt, numAtual, quantidadeNumeros, contador=0;
+        boolean crescente=true;
+
+        System.out.println("Quantos numeros deseja inserir: ");
+        quantidadeNumeros= input.nextInt();
+
+        System.out.println("Numero anterior: ");
+        numAnt
 
 
         }
--- FichasJava/src/FichaPratica03/Ex_14.java
+++ FichasJava/src/FichaPratica03/Ex_14.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Ex_14 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numero, fatorial
+        int numero, fatorial=0;
 
       //ler numero
         System.out.println("Insira numero: ");
         numero = input.nextInt();
 
-        while()
+        while(numero>0){
+            fatorial=numero;
+            numero--;
+        }
+
+        }
 
 
 
